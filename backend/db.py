9b7ae5c8e7ba7303724a4c13572a77025db19c82
dmiv <dmitriy40647274@gmail.com>
2025-03-30 03:22:57 +0300

    db

--- backend/db.py
+++ backend/db.py
@@ -8,7 +8,12 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,  # Increase the pool size
+    max_overflow=20,  # Allow more connections to overflow
+    pool_timeout=30,  # Timeout for getting a connection from the pool
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
